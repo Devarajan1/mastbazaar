9915587e241c1d9160bc91c3b9ebb0e248f2b4be
Devarajan1 <120020691+Devarajan1@users.noreply.github.com>
2023-10-10 12:59:47 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -36,10 +36,6 @@ function Page() {
   useEffect(() => {
     if (locationLattitude !== null) {
       postIp()
-      var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 }, // Set initial map center
-        zoom: 8 // Set initial zoom level
-      });
     }
   }, [locationLattitude])
 
@@ -87,7 +83,7 @@ function Page() {
       </div>
       <div className="w-[55%] h-full flex flex-col  justify-evenly">
         <p className="text-5xl  font-semibold ">Areas We Serving</p>
-        <p className="text-3xl  "><p>We&apos;re delivering freshness at your doorstep!</p></p>
+        <p className="text-3xl  ">We&apos;re delivering freshness at your doorstep!</p>
         <div className="flex ">
           <Command onChange={handleInputChange} className="rounded-lg w-[70%]">
             <CommandInput className=" w-full  border-2 border-[#d3d2d2] rounded-full py-3 px-6 outline-none" placeholder="Type a command or search..." />
@@ -112,12 +108,12 @@ function Page() {
             <Badge variant="outline" key={index} className="m-3 ml-0 bg-[#f7f7f7] justify-center px-6 py-2"><div><p>{Areas}<span className="ml-2 p-1 bg-[#e1e1e1] rounded-full">{ServiceCount[index]}</span></p></div></Badge>))}
         </div>
         <div className="w-[95%]  h-[30%] bg-[#f7f7f7] border-[.05rem] border-[#cccccc] rounded-sm flex  justify-around"  >
-          {locationLattitude ? <div id="map" className="w-full h-full"></div>
-            : <><div className=" flex flex-col w-[50%] justify-center ml-6">
+          
+             <><div className=" flex flex-col w-[50%] justify-center ml-6">
               <p className="text-[#555555] text-2xl font-bold ">View Your Area</p>
               <p className=" text-[#555555] text-lg">Please Select any of the above region</p>
             </div>
-              <Image className="w-[40%] h-[80%] " src={LocationIcon} alt="Map Icon" /></>}
+              <Image className="w-[40%] h-[80%] " src={LocationIcon} alt="Map Icon" /></>
         </div>
       </div>
     </div>
